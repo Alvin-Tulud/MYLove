f0376c7c33ec8e5909dd390d072a945e453d3475
Alvin-Tulud <alvintulud@gmail.com>
2025-02-19 17:56:05 -0800

    sprites

--- MY!LOVE!/Assets/Prefabs/Player/PlayerMove.cs
+++ MY!LOVE!/Assets/Prefabs/Player/PlayerMove.cs
@@ -2,15 +2,37 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    public float moveSpeed = 5.0f;
+    public bool canMove = true;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (canMove)
+        {
+            if (Input.GetKey(KeyCode.A))
+            {
+
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+
+            }
+            if (Input.GetKey(KeyCode.W))
+            {
+
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+
+            }
+        }
     }
 }
